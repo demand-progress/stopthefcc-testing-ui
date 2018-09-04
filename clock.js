91e2ee8799e854087f8339cde8c1ed11df442ece
Mateo Balcorta <misterrattail@gmail.com>
2018-09-04 10:07:35 -0700

    Update module name being required.

--- clock.js
+++ clock.js
@@ -1,9 +1,9 @@
 let CronJob = require('cron').CronJob;
-let bot = require('./worker.js');
+let worker = require('./worker.js');
 
 let job = new CronJob({
-  cronTime: '5 10,11,16,20,22 * * *', // everyday, 9:13, 11:13, 4:13, 8:13,
-  onTick: bot.start(),
+  cronTime: '15 10,11,16,20,22 * * *', // everyday, 9:13, 11:13, 4:13, 8:13,
+  onTick: worker.start(),
   start: true,
   timeZone: 'America/Los_Angeles',
 });
