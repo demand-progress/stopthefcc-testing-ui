0fdc4d8c53e80cf2dd327762995b90d54d26942f
Mateo Balcorta <misterrattail@gmail.com>
2018-09-04 11:55:05 -0700

    Add cron job to run ui testing.:

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cron = require('node-cron');
+const shell = require('./child_helper');
 
 const app = express();
 const cors = require('cors');
@@ -21,10 +23,29 @@ app.options('*', cors());
 
 const port = process.env.PORT || 3000;
 
-const startServer = () => {
-  return app.listen(port, () => {
-    console.log(`The app is listening on port ${port}`);
-  });
-};
+cron.schedule('*/20 * * * * *', () => {
+  console.log('running a task every minute');
+  let commandList = [
+    'npm run test',
+  ];
 
-module.exports = startServer;
+  shell.series(commandList, (err) => {
+    if(err) {
+      console.log('Error: ', err);
+    } else {
+      console.log('Site is working as expected');
+    }
+  });
+});
+
+
+app.listen(port, () => {
+  console.log(`The app is listening on port ${port}`);
+});
+// const startServer = () => {
+//   return app.listen(port, () => {
+//     console.log(`The app is listening on port ${port}`);
+//   });
+// };
+
+module.exports = app;
