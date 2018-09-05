ff7f1b0f6280b8f4d46bf4f27bd5327f761b77bc
Mateo Balcorta <misterrattail@gmail.com>
2018-09-05 15:20:42 -0700

    Clean up.

--- server.js
+++ server.js
@@ -1,11 +1,8 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const cron = require('node-cron');
-const shell = require('./child_helper');
 
 const app = express();
 const cors = require('cors');
-// const routes = require('./routes/routes.js');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -19,31 +16,9 @@ app.use(cors(corsOptions));
 
 app.options('*', cors());
 
-// routes(app);
-
-// cron.schedule('*/20 * * * * *', () => {
-//   console.log('running a task every minute');
-//   let commandList = [
-//     'npm run test',
-//   ];
-
-//   shell.series(commandList, (err) => {
-//     if(err) {
-//       console.log('Error: ', err);
-//     } else {
-//       console.log('Site is working as expected');
-//     }
-//   });
-// });
-
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`The app is listening on port ${port}`);
 });
-// const startServer = () => {
-//   return app.listen(port, () => {
-//     console.log(`The app is listening on port ${port}`);
-//   });
-// };
 
 module.exports = app;
