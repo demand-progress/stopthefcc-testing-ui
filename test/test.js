836c848536487fd112c834797a7cd05c191e5d93
Mateo Balcorta <misterrattail@gmail.com>
2018-09-23 14:33:12 -0700

    Add test for nomobilemegamerger site.

--- test/test.js
+++ test/test.js
@@ -3,15 +3,15 @@ const { expect } = require('chai');
 const { takeAndCompareScreenshot } = require('./compareScreenShots.js');
 const { sendEmail } = require('./sendMail.js');
 const fs = require('fs');
-  puppeteer = require('puppeteer');
+puppeteer = require('puppeteer');
 
 describe('👀 screenshots are correct', () => {
   let browser; let page;
 
   before(async () => {
-    if (!fs.existsSync('testDir')) fs.mkdirSync('testDir');
+    // if (!fs.existsSync('testDir')) fs.mkdirSync('testDir');
 
-    if (!fs.existsSync('testDir/wide')) fs.mkdirSync('testDir/wide');
+    if (!fs.existsSync('test/liveSiteImages')) fs.mkdirSync('test/liveSiteImages');
   });
 
   beforeEach(async () => {
@@ -24,12 +24,39 @@ describe('👀 screenshots are correct', () => {
 
   describe('When comparing live site screen shot to original screen shot', () => {
     beforeEach(async () => page.setViewport({ width: 1280, height: 800 }));
-    it('both should be the same image', async () => {
-      const comparedValue = await takeAndCompareScreenshot(page, 'view1', 'wide');
+    it('stopthefcc live site logos should be the same', async () => {
+      const options = {
+        path: 'test/liveSiteImages/stopthefcc.png',
+        fullPage: false,
+        clip: {
+          x: 0,
+          y: 2250,
+          width: 1280,
+          height: 2100,
+        },
+      };
+      const comparedValue = await takeAndCompareScreenshot(page, 'https://stopthefcc.net/', 'liveSiteImages', 'stopthefcc', options);
       if (comparedValue > 0 || comparedValue < 0) {
         sendEmail('mateo@demandprogress.org');
       }
       return comparedValue;
     }).timeout(6000);
+    it('nomobilemegamerger live site logos should be the same', async () => {
+      const options = {
+        path: 'test/liveSiteImages/nomobilemegamerger.png',
+        fullPage: false,
+        clip: {
+          x: 0,
+          y: 2320,
+          width: 1280,
+          height: 200,
+        },
+      };
+      const comparedValue = await takeAndCompareScreenshot(page, 'https://nomobilemegamerger.com/demo', 'liveSiteImages', 'nomobilemegamerger', options);
+      if (comparedValue > 0 || comparedValue < 0) {
+        sendEmail('mateo@demandprogress.org');
+      }
+      return comparedValue;
+    }).timeout(9000);
   });
 });
