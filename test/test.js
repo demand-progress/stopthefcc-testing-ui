dba45df42cb360973681dbcba48a70fbb4471bb7
Mateo Balcorta <misterrattail@gmail.com>
2018-09-27 10:48:43 -0700

    Clean up after each test on browser.

--- test/test.js
+++ test/test.js
@@ -13,29 +13,34 @@ describe('👀 screenshots are correct', () => {
   });
 
   after(async () => {
-    const removeImages = image => new Promise((resolve) => {
-      fs.unlink(path.join(__dirname, `./livesiteimages/${image}`), () => {
-        resolve(image);
-      });
-    });
-    Promise.all([removeImages('nomobilemegamerger.png'), removeImages('stopthefcc.png'), removeImages('stopthewar.png')]).then((image) => {
-      console.log(`${image} has been removed`);
-    });
+    // const removeImages = image => new Promise((resolve) => {
+    //   fs.unlink(path.join(__dirname, `./livesiteimages/${image}`), () => {
+    //     resolve(image);
+    //   });
+    // });
+    // Promise.all([removeImages('nomobilemegamerger.png'), removeImages('stopthefcc.png'), removeImages('stopthewar.png')]).then((image) => {
+    //   console.log(`${image} has been removed`);
+    // });
   });
 
-  beforeEach(async () => {
-    browser = await puppeteer.launch({ args: ['--no-sandbox', '--disable-setuid-sandbox'] });
-    page = await browser.newPage();
-  });
+  // beforeEach(async () => {
+  //   browser = await puppeteer.launch({ args: ['--no-sandbox', '--disable-setuid-sandbox'] });
+  //   page = await browser.newPage();
+  // });
 
-  // This is ran after every test; clean up after your browser.
-  afterEach(() => {
-    browser.close();
-  });
+  // // This is ran after every test; clean up after your browser.
+  // afterEach(() => {
+  //   browser.close();
+  // });
 
   describe('When comparing live site screen shot to original screen shot', () => {
     beforeEach(async () => {
+      browser = await puppeteer.launch({ args: ['--no-sandbox', '--disable-setuid-sandbox'] });
+      page = await browser.newPage();
       page.setViewport({ width: 1280, height: 800 });
+    });
+    afterEach(() => {
+      browser.close();
     });
     it('stopthefcc live site logos should be the same', async () => {
       const fileName = 'stopthefcc';
