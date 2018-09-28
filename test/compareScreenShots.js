55ce394db28ad590137902a61ca85ff0da079561
Mateo Balcorta <misterrattail@gmail.com>
2018-09-28 12:32:00 -0700

    Increase threshold for pixel match.

--- test/compareScreenShots.js
+++ test/compareScreenShots.js
@@ -16,7 +16,7 @@ const compareScreenshots = (filePath, fileName) => {
       expect(img1.width, 'image widths are the same').equal(img2.width);
       expect(img1.height, 'image heights are the same').equal(img2.height);
       const diff = new PNG({ width: img1.width, height: img2.height });
-      const numDiffPixels = pixelmatch(img1.data, img2.data, diff.data, img1.width, img1.height, { threshold: 1 });
+      const numDiffPixels = pixelmatch(img1.data, img2.data, diff.data, img1.width, img1.height, { threshold: 0.3 });
       if (numDiffPixels > 0 || numDiffPixels < 0) {
         sendEmail('mateo@demandprogress.org', filePath);
       }
